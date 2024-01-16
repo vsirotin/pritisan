f10e8c0a456cef2de0da3b4460c1e19f2d86a390
viktorsirotin <vxod@sirotin.eu>
2024-01-16 17:03:12 +0100

    Design and Implement Language Selection Component Fixes #3. Initial commit.

---
+++ src/app/language-selection/language-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './language-selection.component.html',
+  styleUrl: './language-selection.component.scss'
+})
+export class LanguageSelectionComponent {
+
+}
