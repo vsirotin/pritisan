8dae4b0e2c05494992098c97be472c041e49abf1
viktorsirotin <vxod@sirotin.eu>
2024-02-20 14:56:29 +0100

    Add toolbar with icons to capture component. Data capture basic (momentary event) #32

--- src/app/features/components/capture/capture.component.ts
+++ src/app/features/components/capture/capture.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-capture',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule],
   templateUrl: './capture.component.html',
   styleUrl: './capture.component.scss'
 })
 export class CaptureComponent {
+  title = 'PriTiSAn';
+  isDisabled = true;
+  isPlaying = true;
+
+  onClick() {
+    
+  }
 
 }
