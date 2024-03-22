c7511a393e3decb9badcf681c9cc7fc3120c06b7
viktorsirotin <vxod@sirotin.eu>
2024-03-22 08:33:32 +0100

    Tests definition for component running-events. Capture: running events sub-component #42

--- src/app/features/components/capture/running-events/running-events.component.spec.ts
+++ src/app/features/components/capture/running-events/running-events.component.spec.ts
@@ -21,4 +21,77 @@ describe('RunningActionsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('by empty list of running events', () => {
+    xit('it presented only with header with corresponded text ', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('by not empty list of running events', () => {
+    xit('it presented oppened with header with corresponded text ', () => {
+      expect(component).toBeTruthy();
+    });
+
+    xit('it containts three pairs <label, icon> ', () => {
+      expect(component).toBeTruthy();
+    });
+
+    xit('it can be closed and in closed state its header containts corresponded text ', () => {
+      expect(component).toBeTruthy();
+    });
+
+    xit('Complete selected works correctly by single selection ', () => {
+      expect(component).toBeTruthy();
+    });
+
+    xit('Complete selected works correctly by multiply selection ', () => {
+      expect(component).toBeTruthy();
+    });
+
+    xit('Delete selected works correctly by single selection ', () => {
+      expect(component).toBeTruthy();
+    });
+
+    xit('Delete selected works correctly by multiply selection ', () => {
+      expect(component).toBeTruthy();
+    });
+
+    xit('Cancel/reset works correctly by single selection ', () => {
+      expect(component).toBeTruthy();
+    });
+
+    xit('Cancel/reset works correctly by multiply selection ', () => {
+      expect(component).toBeTruthy();
+    });
+
+    
+    describe('by a few  running events', () => {
+      xit('it containts N lines by count running events N <=3 ', () => {
+          expect(component).toBeTruthy();
+        });
+  
+    });
+    
+    describe('by a many running events', () => {
+    
+      xit('it containts 3 lines by count running events N > 3 ', () => {
+        expect(component).toBeTruthy();
+      });
+    });
+
+    describe('by change a list of running events its presentation changes correctly', () => {
+    
+      xit('it containts right number of lines ', () => {
+        expect(component).toBeTruthy();
+      });
+
+      xit('it is correctly ordered ', () => {
+        expect(component).toBeTruthy();
+      });
+
+    });
+
+  });
 });
+
